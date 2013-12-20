c8ae9544cb9b5de77b5567e37b1989d439e12a9b
aaronwolen <aaron@wolen.com>
2013-12-20 16:16:57 -0500

    serial.enrich: Use a much faster matrix-based approach

--- R/enrichment.r
+++ R/enrichment.r
@@ -10,20 +10,17 @@
 #'         
 
 calc.enrich <- function(object, stat, thresh.levels) {
-  
+
   if (missing(thresh.levels)) {
     thresh.levels <- quantile(stat, seq(0, 1, 0.1))
   }
   
   out <- lapply(features(object), function(f) 
                  lapply(f, serial.enrich, stat, thresh.levels))
-  
+
   out <- melt(out, measure.vars = NULL)
-  out <- subset(out, feature == TRUE, select = -feature)
   out <- rename(out, c(L1 = "feature", L2 = "sample"))
-  levels
-  out$threshold <- factor(out$threshold, 
-                          sort(as.numeric(levels(out$threshold))))
+  out$threshold <- factor(out$threshold)
   return(out)
 }
 
@@ -35,34 +32,20 @@ calc.enrich <- function(object, stat, thresh.levels) {
 #'        be applied for each enrichment calculation
 #' 
 #' @return data.frame containing `enrichment`
-#' @importFrom plyr ddply as.quoted
 #' @export
 
 serial.enrich <- function(feature, stat, thresh.levels) {
   
-  # Perform counts across all thresholds
-  base.counts <- data.frame(table(feature))
-  names(base.counts) <- c("feature", "count")
-  base.counts$prop <- base.counts$count / sum(base.counts$count)
+  n <- length(stat)
+  thresh.levels <- unique(c(0, thresh.levels))
+  thresh.levels <- matrix(rep(thresh.levels, each = n), nrow = n)
 
-  thresh.counts <- lapply(thresh.levels, function(t) table(feature[stat >= t]))
-  names(thresh.counts) <- thresh.levels
-  thresh.counts <- thresh.counts[sapply(thresh.counts, dim) > 0]
+  hits.mat <- feature & (stat >= thresh.levels)
   
-  thresh.counts <- lapply(thresh.counts, data.frame)
-
-  # Re-format output
-  out <- data.frame(threshold = rep(names(thresh.counts), 
-                                    sapply(thresh.counts, nrow)))
-  out <- data.frame(out, do.call("rbind", thresh.counts))
-  names(out) <- c("threshold", "feature", "count")
-  
-  # Proportion of feature at each threshold
-  out <- ddply(out, as.quoted("threshold"), transform, 
-               prop = count / sum(count, na.rm = TRUE))
-  
-  base.index <- match(out$feature, base.counts$feature)
-  out$enrichment <- out$prop / base.counts$prop[base.index]
+  out <- data.frame(threshold = thresh.levels[1,],
+                        count = colSums(hits.mat),
+                         prop = colMeans(hits.mat))
+  out$enrichment <- out$prop / mean(feature)
   
   return(out)
 }
