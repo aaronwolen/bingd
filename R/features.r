3e9ad3b38b57157bff5158c7fc7bc51393bdc205
aaronwolen <aaron@wolen.com>
2014-01-07 16:15:51 -0500

    feature.search: Enable to work offline
    * Add get.metadata function
    * Add feature.labels functions

--- R/features.r
+++ R/features.r
@@ -1,21 +1,17 @@
 #' Search AnnotationHub for features
 #' 
 #' @param filter named list of vectors
+#' @inheritParams get.metadata
 #' 
-#' @importFrom AnnotationHub AnnotationHub metadata
 #' @export
 
-feature.search <- function(data.filter, genome, hub, md) {
+feature.search <- function(data.filter, genome, md, online = NULL) {
 
-  if (missing(md)) {
-    if(missing(hub)) {
-      hub <- AnnotationHub()
-    }
-    md <- metadata(hub)
-  }
+  if (missing(md)) md <- get.metadata(online = online)
   
   # Filter based on genome
-  if (!missing(genome)) md <- md[md$Genome %in% genome,]
+  if (!missing(genome) & "genome" %in% colnames(md)) 
+    md <- md[md$Genome %in% genome,]
   
   # Apply user specified filters
   mgrep <- function(pattern, x, ignore.case = TRUE, ...) {
@@ -28,3 +24,39 @@ feature.search <- function(data.filter, genome, hub, md) {
   
   return(filter.hits)
 }
+
+
+#' Retrieve AnnotationHub metadata
+#' 
+#' @param online logical, should metadata be retrieved online
+#' 
+#' @importFrom AnnotationHub AnnotationHub metadata
+#' @importFrom Biobase testBioCConnection
+
+get.metadata <- function(online = NULL) {
+  
+  if (is.null(online)) online <- testBioCConnection()
+  
+  if (online) {
+    md <- metadata()
+  } else {
+    path <- AnnotationHub:::hubCache()
+    md <- dir(file.path(path, "resources"), recursive = TRUE)
+    md <- DataFrame(RDataPath = md, Title = feature.labels(md))
+    rownames(md) <- NULL # DataFrame (1.20.6) doesn't respect row.names = NULL
+  }
+  
+  return(md)
+}
+
+#' Create pretty feature labels from the full RDataPaths
+#' 
+#' @param x characer vector of full RDataPaths
+
+feature.labels <- function(x) {
+  out <- basename(x)
+  out <- sapply(strsplit(out, "\\.", fixed = F), function(x) x[1])
+  names(out) <- x
+  return(out)  
+}
+
