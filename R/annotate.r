e62cfec029c0b0a470cf807d26638aa087095ca7
aaronwolen <aaron@wolen.com>
2014-01-07 17:48:36 -0500

    annotate.gwas: Enable to work offline using load.feature

--- R/annotate.r
+++ R/annotate.r
@@ -11,21 +11,20 @@
 #' @examples
 #' scz <- annotate.gwas(scz, data.filter = list(DNaseI = c("UwDnaseN", "broadPeak")))
 
-annotate.gwas <- function(gwas, data.filter) {
-
-  message("Creating AnnotationHub object...")
-  hub <- AnnotationHub()
-  md <- metadata(hub)
+annotate.gwas <- function(gwas, data.filter, hub = NULL, ...) {
   
-  filter.hits <- feature.search(data.filter, genome(gwas)[1], hub, md)
+  filter.hits <- feature.search(data.filter, genome(gwas)[1], ...)
   
   # Retrieve features and check for gwas overlaps
   message("Annotating GWAS markers...")
   overlaps <- mclapply(unlist(filter.hits), function(f)
-                     gwas %over% AnnotationHub:::.getResource(hub, f),
+                     gwas %over% load.feature(f, hub = hub),
                      mc.cores = getDoParWorkers())
   
-  names(overlaps) <- md$Description[match(unlist(filter.hits), make.names(md$RDataPath))]
+  # Use metadata labels
+  md <- get.metadata(online = FALSE)
+  md.index <- sapply(unlist(filter.hits), grep, make.names(md$RDataPath))
+  names(overlaps) <- md$Title[md.index]
   
   # It'd be nice if overlap results for each feature type could
   # be stored in different slots but for now all features are just
