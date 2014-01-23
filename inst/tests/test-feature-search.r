590a589d1f95204631a254f56d2cb02929ce7a94
aaronwolen <aaron@wolen.com>
2014-01-23 10:29:37 -0500

    tests: Add tests for hub.search

---
+++ inst/tests/test-feature-search.r
@@ -0,0 +1,14 @@
+context("Feature search")
+
+test_that("AnnotationHub feature search works offline", {
+  query <- list(Tier1 = c("Gm12878", "Encode"))
+  cache.dir <- file.path(system.file(package = "bingd"), "data", "resources")
+  
+  list.hit <- hub.search(query, online = FALSE, cache.dir = cache.dir)
+  atomic.hit <- hub.search(query[[1]], online = FALSE, cache.dir = cache.dir)
+  
+  expect_identical(list.hit[[1]], atomic.hit[[1]])
+  
+  expect_equal(names(list.hit[[1]]), c("Title", "LocalPath"))
+  expect_equal(names(atomic.hit[[1]]), c("Title", "LocalPath"))  
+})
