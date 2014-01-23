f8a9003c59db559a641c5eb4bcced66ee595b589
aaronwolen <aaron@wolen.com>
2014-01-23 11:56:47 -0500

    tests: Add online hub.search() tests

--- inst/tests/test-feature-search.r
+++ inst/tests/test-feature-search.r
@@ -1,15 +1,25 @@
 context("Feature search")
 
+query <- list(Tier1 = c("Gm12878", "Encode"))
+cache.dir <- system.file("data", package = "bingd")
+required.cols <- c("Title", "LocalPath")
+
 test_that("AnnotationHub feature search works offline", {
-  query <- list(Tier1 = c("Gm12878", "Encode"))
-  
-  cache.dir <- file.path(system.file("data", package = "bingd"), "resources")
   
   list.hit <- hub.search(query, online = FALSE, cache.dir = cache.dir)
   atomic.hit <- hub.search(query[[1]], online = FALSE, cache.dir = cache.dir)
   
   expect_identical(list.hit[[1]], atomic.hit[[1]])
   
-  expect_equal(names(list.hit[[1]]), c("Title", "LocalPath"))
-  expect_equal(names(atomic.hit[[1]]), c("Title", "LocalPath"))  
+  expect_equal(names(list.hit[[1]]), required.cols)
+  expect_equal(names(atomic.hit[[1]]), required.cols)  
+})
+
+
+test_that("AnnotationHub feature search works online", {
+  
+  online.hit <- hub.search(query, online = TRUE, cache.dir = cache.dir)
+  
+  expect_true(all(required.cols %in% names(online.hit[[1]])))
+  expect_equal(nrow(online.hit[[1]]), 2)  
 })
