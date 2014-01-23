ce2e6fab8a03b7917f128ff552d21776aef1152a
aaronwolen <aaron@wolen.com>
2014-01-23 14:43:28 -0500

    features: Move features code to gwas-methods.r

--- R/utils.r
+++ R/utils.r
@@ -1,32 +1,3 @@
-#' Accessor for feature annotations stored in gwas object
-#' 
-#' Only needed until feature annotations are stored hierarchically by type
-#' 
-#' @param gwas GWAS GRanges object
-#' 
-#' @export
-#' 
-#' @return 
-#' List of data.frames in which each slot corresponds to a feature type
-
-features <- function(object) {
-  
-  md.names <- names(mcols(object))
-  feature.cols <- md.names[grep("^\\.", md.names)]
-  feature.vars <- do.call("rbind", strsplit(feature.cols, "\\."))
-  feature.types <- feature.vars[, 2]
-  feature.names <- split(feature.vars[, 3], feature.types)
-  
-  out <- split(feature.cols, feature.types)
-  out <- lapply(out, function(x) mcols(object)[x])
-  
-  out <- mapply(function(x, y) {
-                  names(x) <- y; x
-                }, out, feature.names, SIMPLIFY = FALSE)
-  return(out)
-}
-
-
 #' Fix a GRanges object's chromosome information
 #' 
 #' Edit a GRanges object's chromosome information by adding chromosome lengths 
