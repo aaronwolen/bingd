a3ae02145ceb19c92f1251461cbfb6aa8d980838
aaronwolen <aaron@wolen.com>
2014-01-30 15:54:25 -0500

    tests: Fix bug in GWAS-class tests

--- inst/tests/test-gwas-class.r
+++ inst/tests/test-gwas-class.r
@@ -1,9 +1,10 @@
 context("GWAS classes")
 
-n.markers <- 100
+test.chrs <- rep(paste0("chr", c(1:22, "X")), each = 100)
+n.markers <- length(test.chrs)
+
 test.snps <- paste0("rs", seq_len(n.markers))
-test.chrs <- rep(paste0("chr", c(1:22, "X")), each = n.markers)
-test.pos  <- round(runif(n.markers, 1, 100e6), 0)
+test.pos  <- round(runif(test.chrs, 1, 100e6), 0)
 test.pval <- runif(n.markers)
 test.or   <- rnorm(n.markers)
 
@@ -11,22 +12,21 @@ test_that("GWAS object from data.frame", {
  
   test.df <- data.frame(test.snps, test.chrs, test.pos, test.pval, test.or)
   
-  test.gwas <- as.GWAS(test.df,
-                       marker = "test.snps", chr = "test.chrs", bp = "test.pos", 
-                       pvalue = "test.pval", or = "test.or")
+  gwas.df <- as.GWAS(test.df,
+                     marker = "test.snps", chr = "test.chrs", bp = "test.pos", 
+                     pvalue = "test.pval", or = "test.or")
   
-  expect_match(class(gwas), "GWAS")
+  expect_match(class(gwas.df), "GWAS")
 })
 
 
 
 test_that("GWAS object from GRanges", {
  
-  test.gr <- GRanges(test.snps, IRanges(test.pos, width = 1), strand = "*",
+  test.gr <- GRanges(test.chrs, IRanges(test.pos, width = 1), strand = "*",
                      test.snps, test.pval, test.or)
   
-  gwas <- as.GWAS(test.gr,
-                  marker = "test.snps", pvalue = "test.pval", or = "test.or")
-  expect_match(class(gwas), "GWAS")
+  gwas.gr <- as.GWAS(test.gr,
+                     marker = "test.snps", pvalue = "test.pval", or = "test.or")
+  expect_match(class(gwas.gr), "GWAS")
 })
-.
