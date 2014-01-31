2af05f1a3af81959bb9269fc56e9b3cfeeb9b009
aaronwolen <aaron@wolen.com>
2014-01-31 17:54:25 -0500

    Add function to check for presence and class of required columns

--- R/checks.r
+++ R/checks.r
@@ -6,3 +6,18 @@ is.FeatureList <- function(x) {
   return(x)
 }
  
+
+#' Verify the presence and class of a data.frame variable
+#' 
+#' @param object \code{data.frame} or a \code{\link[IRanges]{DataFrame}}
+#' @param description description of class being tested to print in error
+#' @param var.name column or slot name to check for
+#' @param var.class expected class of column or slot
+
+.validColClass <- function(object, description, var.name, var.class) {
+  if (!var.name %in% colnames(object)) 
+    stop(description, " must contain ", var.name, call. = FALSE)
+  
+  if (var.class != class(object[[var.name]]))
+    stop(var.name, " in ", description, " should be ", var.class, call. = FALSE)
+}
