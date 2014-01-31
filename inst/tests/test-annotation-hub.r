4ee8c068492c6399e795f69a012680091da14b80
aaronwolen <aaron@wolen.com>
2014-01-31 11:08:28 -0500

    tests: Run cache.features outside of test so the updated features object can be used downstream

--- inst/tests/test-annotation-hub.r
+++ inst/tests/test-annotation-hub.r
@@ -16,15 +16,20 @@ test_that("Searching for uncached features", {
 
 
 
-test_that("Downloading uncached features", {
+test_that("Test features are uncached", {
   
   # Files aren't cached
   expect_equal(sum(sapply(features, function(x) x$Cached)), 0)
   feature.files <- unlist(lapply(features, function(x) x$LocalPath))
   expect_false(any(file.exists(feature.files)))
+})
   
-  # Files are cached
-  features <- cache.features(features, test.dir)
+
+
+# Cache test features
+features <- cache.features(features, test.dir)
+
+test_that("Test features were cached", {
   
   # Returns a FeatureList
   expect_identical(is.FeatureList(features), features)
@@ -35,5 +40,3 @@ test_that("Downloading uncached features", {
   # FeatureList indicates files are cached
   expect_equal(sum(sapply(features, function(x) x$Cached)), 4)
 })
-
-
