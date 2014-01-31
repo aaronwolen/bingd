44d01e0f1c175369e94b93aea2332820621c394e
aaronwolen <aaron@wolen.com>
2014-01-31 17:59:19 -0500

    cache.features: Move to FeatureList-methods

---
+++ FeatureList-methods.r
@@ -0,0 +1,48 @@
+#' Download uncached features
+#' 
+#' @inheritParams annotate.gwas
+#' @inheritParams local.features
+#' 
+#' @importFrom AnnotationHub AnnotationHub
+#' @export
+#' 
+#' @return \code{FeatureList}
+
+cache.features <- function(feature.list, path) {
+  feature.list <- is.FeatureList(feature.list)
+  if (missing(path)) path <- cache.path()
+  
+  is.uncached <- function(x) x[!x$Cached, "LocalPath"]
+  uncached <- lapply(feature.list, is.uncached)
+  
+  uncached.files <- basename(unlist(uncached))
+  
+  hub <- AnnotationHub(hubCache = path)
+  hub.files <- hub@snapshotPaths
+    
+  # Download uncached files
+  cached.files <- character()
+  for (file in uncached.files) {
+    
+    hub.file <- hub.files[match(file, basename(hub.files))]
+    
+    # Report unmatched files
+    if (is.na(hub.file)) {
+      warning(file, " not found on AnnotationHub.\b", call. = F)
+    }
+    
+    dl <- AnnotationHub:::.downloadFile(hub, hub.file)
+    
+    if (dl == 0) {
+      cached.files <- c(cached.files, file)
+    } else {
+      stop("Failed to download:\n\t", file, call. = FALSE)
+    }
+  }
+  
+  feature.list <- Map(function(f) {
+    f$Cached[basename(f$LocalPath) %in% cached.files] <- TRUE; f
+  }, feature.list)
+  
+  return(as.FeatureList(feature.list))
+}
