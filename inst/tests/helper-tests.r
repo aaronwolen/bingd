78c213ed1272c84565eec746ed212ca35fcf435f
aaronwolen <aaron@wolen.com>
2014-01-31 11:23:56 -0500

    tests: Add a function to check if the test directory already exists

--- inst/tests/helper-tests.r
+++ inst/tests/helper-tests.r
@@ -1,5 +1,15 @@
 # Create a test cache directory
-test.dir <- paste(cache.path(), "test", sep = "-")
+make_test <- function(dir.name) {
+  if (missing(dir.name)) dir.name <- paste(cache.path(), "test", sep = "-")
+  if (file.exists(dir.name)) {
+    warning(dir.name, " already exists.")
+    rndm.ext <- paste(sample(letters, 3), collapse = "")
+    dir.name <- paste(dir.name, rndm.ext, sep = "-")
+  }
+  return(dir.name)
+}
+
+test.dir <- make_test()
 
 query <- list(DNase   = c("GM12878", "UwDnase", "narrowPeak"),
               Histone = c("GM12878", "H3k4me3", "narrowPeak"))
