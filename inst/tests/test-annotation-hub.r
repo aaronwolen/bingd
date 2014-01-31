308c61ae15a3e76d2298ea2007c70ab7a6202df6
aaronwolen <aaron@wolen.com>
2014-01-31 11:44:47 -0500

    tests: Improve consistency of testing for cached and uncached features

--- inst/tests/test-annotation-hub.r
+++ inst/tests/test-annotation-hub.r
@@ -9,7 +9,7 @@ test_that("Searching for uncached features", {
   
   # Specified cache directory was created
   expect_true(file.exists(test.dir))
-
+  
   # Returns a FeatureList
   expect_identical(is.FeatureList(features), features)
 
@@ -22,9 +22,12 @@ test_that("Searching for uncached features", {
 
 test_that("Test features are uncached", {
   
-  # Files aren't cached
+  # FeatureList indicates files aren't cached
   expect_equal(sum(sapply(features, function(x) x$Cached)), 0)
+  
+  # Files don't exist
   feature.files <- unlist(lapply(features, function(x) x$LocalPath))
+  expect_true(all(grepl(test.dir, feature.files)))
   expect_false(any(file.exists(feature.files)))
 })
   
@@ -38,9 +41,11 @@ test_that("Test features were cached", {
   # Returns a FeatureList
   expect_identical(is.FeatureList(features), features)
   
-  # Files are now cached
-  expect_true(all(file.exists(feature.files)))
-  
   # FeatureList indicates files are cached
   expect_equal(sum(sapply(features, function(x) x$Cached)), 4)
+  
+  # Files do exist
+  feature.files <- unlist(lapply(features, function(x) x$LocalPath))
+  expect_true(all(grepl(test.dir, feature.files)))
+  expect_true(all(file.exists(feature.files)))
 })
