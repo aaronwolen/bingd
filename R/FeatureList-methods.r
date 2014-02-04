159c3df373589b6996286543e3f59fb2d3bd8e87
aaronwolen <aaron@wolen.com>
2014-02-03 19:29:10 -0500

    cache.features: Update to work with S4 FeatureList

--- R/FeatureList-methods.r
+++ R/FeatureList-methods.r
@@ -24,41 +24,49 @@ setMethod("show", "FeatureList",
 #' 
 #' @return \code{FeatureList}
 
-cache.features <- function(feature.list, path) {
-  feature.list <- is.FeatureList(feature.list)
-  if (missing(path)) path <- cache.path()
-  
-  is.uncached <- function(x) x[!x$Cached, "LocalPath"]
-  uncached <- lapply(feature.list, is.uncached)
-  
-  uncached.files <- basename(unlist(uncached))
-  
-  hub <- AnnotationHub(hubCache = path)
-  hub.files <- hub@snapshotPaths
+setGeneric("cache.features", 
+  function(object, path) {
+    standardGeneric("cache.features")
+})
+           
+setMethod("cache.features", "FeatureList",
+  function(object, path) {
     
-  # Download uncached files
-  cached.files <- character()
-  for (file in uncached.files) {
+    if (missing(path)) path <- cache.path()
     
-    hub.file <- hub.files[match(file, basename(hub.files))]
+    object <- stack(object)
+    uncached.files <- basename(subset(object, !Cached)$LocalPath)
     
-    # Report unmatched files
-    if (is.na(hub.file)) {
-      warning(file, " not found on AnnotationHub.\b", call. = F)
+    hub <- AnnotationHub(hubCache = path)
+    hub.files <- hub@snapshotPaths
+      
+    # Download uncached files
+    cached.files <- character()
+    for (file in uncached.files) {
+      
+      hub.file <- hub.files[match(file, basename(hub.files))]
+      
+      # Report unmatched files
+      if (is.na(hub.file)) {
+        warning(file, " not found on AnnotationHub.\b", call. = F)
+      }
+      
+      dl <- AnnotationHub:::.downloadFile(hub, hub.file)
+      
+      if (dl == 0) {
+        cached.files <- c(cached.files, file)
+      } else {
+        stop("Failed to download:\n\t", file, call. = FALSE)
+      }
     }
     
-    dl <- AnnotationHub:::.downloadFile(hub, hub.file)
+    object$Cached[basename(object$LocalPath) %in% cached.files] <- TRUE
+    object <- lapply(split(object, object$name), subset, select = -name)
     
-    if (dl == 0) {
-      cached.files <- c(cached.files, file)
-    } else {
-      stop("Failed to download:\n\t", file, call. = FALSE)
-    }
-  }
-  
-  feature.list <- Map(function(f) {
-    f$Cached[basename(f$LocalPath) %in% cached.files] <- TRUE; f
-  }, feature.list)
-  
-  return(as.FeatureList(feature.list))
-}
+    return(FeatureList(object))
+})
+
+
+# Subsetting --------------------------------------------------------------
+
+#' Return 
