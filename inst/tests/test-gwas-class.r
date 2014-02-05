e6c7ba39ac0f48c8bf65515377433f196127d5de
aaronwolen <aaron@wolen.com>
2014-02-05 12:41:02 -0500

    tests: Add tests for the GWAS object genome requirement

--- inst/tests/test-gwas-class.r
+++ inst/tests/test-gwas-class.r
@@ -8,41 +8,58 @@ test.pos  <- round(runif(test.chrs, 1, 100e6), 0)
 test.pval <- runif(n.markers)
 test.or   <- rnorm(n.markers)
 
-test_that("GWAS object from data.frame", {
- 
-  test.df <- data.frame(test.snps, test.chrs, test.pos, test.pval, test.or)
-  
-  gwas.df <- as.GWAS(test.df,
-                     marker = "test.snps", chr = "test.chrs", bp = "test.pos", 
-                     pvalue = "test.pval", or = "test.or")
-  
-  expect_match(class(gwas.df), "GWAS")
+test.df <- data.frame(test.snps, test.chrs, test.pos, test.pval, test.or)
+
+test.args <- c(genome = "hg19", 
+               marker = "test.snps", chr = "test.chrs", bp = "test.pos", 
+               pvalue = "test.pval", or = "test.or")
+
+test_that("GWAS creation from GRanges fails without required information", {   
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-1])))
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-2])))
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-3])))
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-4])))
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-5])))
+  expect_error(do.call("as.GWAS", c(list(test.df), test.args[-6])))
 })
 
+test_that("GWAS creation from data.frame", {
+  gwas.df <- do.call("as.GWAS", c(list(test.df), test.args))  
+  expect_match(class(gwas.df), "GWAS")
+})
 
 
 # Create GRanges object
 test.gr <- GRanges(test.chrs, IRanges(test.pos, width = 1), strand = "*",
                    test.snps, test.pval, test.or)
 
-test_that("GWAS creation fails without required information", {
-  expect_error(as.GWAS(test.gr, marker = "test.snps", pvalue = "test.pval"))
-  expect_error(as.GWAS(test.gr, marker = "test.snps", or = "test.or"))
-  expect_error(as.GWAS(test.gr, pvalue = "test.pval", or = "test.or"))
+test.args <- test.args[!names(test.args) %in% c("chr", "bp")]
+
+test_that("GWAS creation from GRanges fails without required information", {
+  expect_error(do.call("as.GWAS", c(list(test.gr), test.args[-1])))
+  expect_error(do.call("as.GWAS", c(list(test.gr), test.args[-2])))
+  expect_error(do.call("as.GWAS", c(list(test.gr), test.args[-3])))
+  expect_error(do.call("as.GWAS", c(list(test.gr), test.args[-4])))
 })
 
 
-
 # Create GWAS object
-gwas.gr <- as.GWAS(test.gr, marker = "test.snps", 
-                   pvalue = "test.pval", or = "test.or")
+gwas.gr <- do.call("as.GWAS", c(list(test.gr), test.args))
 
 test_that("GWAS object from GRanges", {
   expect_match(class(gwas.gr), "GWAS")
 })
 
+test_that("GWAS object using annotated genome", {
+  genome(test.gr) <- "hg19"
+  gwas.gr <- do.call("as.GWAS", c(list(test.gr), test.args[-1]))
+  expect_match(class(gwas.gr), "GWAS")
+})
 
-# Create annotated GWAS object
+
+
+context("Create annotated GWAS object")
+
 features <- local.features(query, path = test.dir)
 gwas.annot <- annotate.gwas(gwas.gr, feature.list = features)
 
@@ -81,7 +98,6 @@ test_that("Enrichment results are valid", {
   # sample column matches FeatureList Titles
   expect_match(unique(enrich$sample), sapply(features, function(x) x$Title))
 })
-
 
 
 test_that("Same results for annotated and unannotated GWAS objects", {  
