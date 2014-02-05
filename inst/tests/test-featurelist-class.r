b90fe6db619b8256b2ce2634c5fe84b6e4ee4bba
aaronwolen <aaron@wolen.com>
2014-02-05 10:47:58 -0500

    tests: Add tests for FeatureList constructor (closes #5)

---
+++ inst/tests/test-featurelist-class.r
@@ -0,0 +1,42 @@
+context("FeatureList class")
+
+df <- data.frame(LocalPath = rep(test.dir, 3),
+                 Title = basename(test.dir),
+                 Cached = TRUE,
+                 stringsAsFactors = FALSE)
+
+test_that("From data.frame", {
+
+  fl1 <- FeatureList(df)
+  expect_match(class(fl1), "FeatureList")
+  expect_match(names(fl1), "features")
+  
+  fl2 <- FeatureList(df, df)
+  expect_match(class(fl2), "FeatureList")
+  expect_match(names(fl2), c("features1", "features2"))
+  
+  fl3 <- FeatureList(label1 = df, label2 = df)
+  expect_match(class(fl3), "FeatureList")
+  expect_match(names(fl3), c("label1", "label2"))
+  
+  expect_identical(FeatureList(df, df, names = c("label1", "label2")), fl3)
+})
+
+test_that("From DataFrame", {
+
+  df <- IRanges::DataFrame(df)
+  
+  fl1 <- FeatureList(df)
+  expect_match(class(fl1), "FeatureList")
+  expect_match(names(fl1), "features")
+  
+  fl2 <- FeatureList(df, df)
+  expect_match(class(fl2), "FeatureList")
+  expect_match(names(fl2), c("features1", "features2"))
+  
+  fl3 <- FeatureList(label1 = df, label2 = df)
+  expect_match(class(fl3), "FeatureList")
+  expect_match(names(fl3), c("label1", "label2"))
+  
+  expect_identical(FeatureList(df, df, names = c("label1", "label2")), fl3)
+})
