8cdf3f7b9f9155190c3ee7f7c3a46d4effdc6504
aaronwolen <aaron@wolen.com>
2014-02-10 16:53:19 -0500

    AnnotatedGWAS-class: Add accessor functions
    * features()
    * fcols()
    * featureIndex()

--- R/AnnotatedGWAS-methods.r
+++ R/AnnotatedGWAS-methods.r
@@ -1,3 +1,46 @@
+#' Access AnnotatedGWAS feature index
+#' 
+#' @param object \code{AnnotatedGWAS} object
+
+setGeneric("featureIndex", function(object) {
+  standardGeneric("featureIndex")
+})
+
+setMethod("featureIndex", "AnnotatedGWAS", function(object) object@featureIndex)
+
+
+#' Extract list of features from AnnotatedGWAS object
+#' 
+#' @inheritParams featureIndex
+
+setGeneric("features", function(object) {
+  standardGeneric("features")
+})
+
+setMethod("features", "AnnotatedGWAS", function(object) {
+  out <- lapply(featureIndex(object), function(x) 
+                rename(mcols(object)[, x], structure(names(x), names = x)))
+  return(DataFrameList(out))
+})
+
+
+#' Extract features from AnnotatedGWAS object
+#' 
+#' @inheritParams featureIndex
+
+setGeneric("fcols", function(object) {
+  standardGeneric("fcols")
+})
+
+setMethod("fcols", "AnnotatedGWAS", function(object) {
+  
+  out <- DataFrame(features(object))
+  names(out) <- paste0(".", names(out))
+  
+  return(out)
+})
+
+
 #' Accessor for feature annotations stored in gwas object
 #' 
 #' Only needed until feature annotations are stored hierarchically by type
