22737884f38e8a099354d86a7279298a9494f8e2
aaronwolen <aaron@wolen.com>
2014-02-14 10:37:23 -0500

    calc.bayes: Initial commit

--- R/bayes-methods.r
+++ R/bayes-methods.r
@@ -1,3 +1,56 @@
+#' Calculate posterior probability of being an effective SNP
+#' 
+#' Integrate GWAS results with annotated genomic features
+#' 
+#' @param object \code{AnnotatedGWAS} object
+#' 
+#' @importFrom plyr ddply
+#' @export
+
+setGeneric("calc.bayes", function(object) {
+  standardGeneric("calc.bayes")
+})
+
+setMethod("calc.bayes", "AnnotatedGWAS", function(object) {
+  
+  gwas.params <- calc.pe(object, trace = 2)
+  cond.probs <- calc.conditionals(object, risk.thresh = 1e-4)
+  
+  post.probs <- data.frame(label = label.groups(fcols(object)),
+                          marker = marker(object),
+                          zscore = zscore(object),
+                             p.e = gwas.params$p.e,
+                             p.n = gwas.params$p.n,
+                           stringsAsFactors = FALSE)
+  
+  
+  # density of zscores for risk SNPs and conditional on null density (inflated)
+  fz_e <- function(z, l) (dnorm((2 - z) / l) + dnorm((-2 - z) / l)) / 2 / l
+  fz_n <- function(z, l)  dnorm(z / l) / l
+  
+  post.probs <- transform(post.probs,
+                          fz.e = fz_e(zscore, gwas.params$lambda), 
+                          fz.n = fz_n(zscore, gwas.params$lambda))
+  
+  # Add conditional feature probabilities
+  l.index <- match(post.probs$label, cond.probs$label)
+  post.probs <- transform(post.probs,
+                          p.f.r = cond.probs$prob[l.index],
+                          p.f.n = cond.probs$prob.base[l.index])
+  
+  # Calculate posterior probabilities by feature combination groups                  
+  post.probs <- ddply(post.probs, "label", transform,
+                      post.prob = (p.f.r * fz.e * p.e) / 
+                                 ((p.f.r * fz.e * p.e) + p.f.n * fz.n * p.n))  
+  
+  m.index <- match(marker(object), post.probs$marker)
+  mcols(object)$post.prob <- post.probs$post.prob[m.index]
+  
+  return(object)
+})
+
+
+
 #' Calculate prior probability of being an effective SNP
 #'
 #' @param object \code{AnnotatedGWAS} object
