882967682fbaeede329cd711ae73f2585114fe1c
aaronwolen <aaron@wolen.com>
2014-02-20 15:48:22 -0500

    Remove deprecated S3 FeatureList class

--- R/class.r
+++
@@ -1,9 +0,0 @@
-#' Create a \code{FeatureList} object
-#' 
-#' @param x a \code{\link{list}} of \code{\link{DataFrame}} objects
-
-as.FeatureList <- function(x) {
-  stopifnot(class(x) == "list")
-  class(x) <- c("FeatureList", class(x))
-  return(x)
-}
