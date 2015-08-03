3041aa39329aa73f752bd2c5e37752efaa6c4c21
aaronwolen <aaron@wolen.com>
2015-08-03 13:00:38 -0400

    Use consistent variable names in AH helpers

--- R/annotation-hub.r
+++ R/annotation-hub.r
@@ -1,19 +1,20 @@
-# Unexported helper functions copied from AnnotationHub 1.6.0
+# Unexported helper functions copied from AnnotationHub
 
 cache.path <- function() AnnotationHub::hubCache()
 
-cache.exists <- function(filePath) 
-  !is.na(filePath) && isTRUE(file.info(filePath)$isdir)
+cache.exists <- function(path) 
+  !is.na(path) && isTRUE(file.info(path)$isdir)
 
-cache.create <- function(dirPath) {
-    if (!file.exists(dirPath))
-        if (!dir.create(dirPath, showWarnings=FALSE, recursive=TRUE)) {
-            warning(gettextf("unable to create %s", sQuote(dirPath)),
-                    domain = NA)
+cache.create <- function(path) {
+    if (!file.exists(path))
+        if (!dir.create(path, showWarnings=FALSE, recursive=TRUE)) {
+            warning(gettextf("unable to create %s", sQuote(path)), domain = NA)
+        } else {
+          AnnotationHub::setHubOption("CACHE", path)
         }
-    else if (!file.info(dirPath)$isdir)
-        stop("path exists but is not a directory: ", sQuote(dirPath))
-    dirPath
+    else if (!file.info(path)$isdir)
+        stop("path exists but is not a directory: ", sQuote(path))
+    path
 }
 
 # appends resources to cache path
