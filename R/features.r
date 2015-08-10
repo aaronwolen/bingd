bab3430bbbd882d6acd6df4afa2864901baefb22
aaronwolen <aaron@wolen.com>
2015-08-10 19:30:28 -0400

    Workaround for AH bug loading RoadMap DNase files

--- R/features.r
+++ R/features.r
@@ -9,7 +9,27 @@ load_feature <- function(object, which, seqlevels = NULL, GNCList = FALSE) {
   if (!is(object, "AnnotationHub")) stop("object must be an AnnotatoinHub object.")
   if (missing(which)) stop("Must provide index or name of AnnotationHub record.")
   
-  obj <- object[[which]]
+  title <- AnnotationHub::mcols(object[which])$title
+  if (grepl("DNase.hotspot.fdr0.01", title, fixed = TRUE)) {
+
+    if (grepl("01.peaks.bed", title, fixed = TRUE)) {
+      extra.cols <- c(name="character", 
+                      signalValue = "numeric", 
+                      pValue = "numeric")
+    } else if (grepl("01.broad.bed", title, fixed = TRUE)) {
+      extra.cols <- c(name="character", 
+                      pValue = "numeric")
+    } else {
+      stop("Encountered unanticipated RoadMap DNase BED file format")
+    }
+    
+    er <- AnnotationHub::cache(object[which])
+    obj <- rtracklayer::import(er, format="bed", extraCols = extra.cols)
+  } else {
+    obj <- object[[which]]  
+  }
+  
+  
   if (!is(obj, "GRanges")) stop("Features must be GRanges objects.")
   
   if (!is.null(seqlevels)) obj <- GenomeInfoDb::keepSeqlevels(obj, seqlevels)
